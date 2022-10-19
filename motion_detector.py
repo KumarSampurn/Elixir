c1a59b460e779bb760757e42c2b94541a479e0e4
Epsilon <101011193+KumarSampurn@users.noreply.github.com>
2022-10-19 18:15:24 +0530

    blue colour motion detector
    Added a blue colour mask and now tracking motion of the object in mask . And since there is only blue colour object in mask therefore it is a blue colour motion detector.
    
    In Line 39 :  Contour area 3000 is way too much and 300 is a little less , need to find the sweet spot in between.

--- motion_detector.py
+++ motion_detector.py
@@ -8,17 +8,34 @@ prev = cv2.flip(prev, 1)
 _, new = cap.read()
 new = cv2.flip(new, 1)
 
+_, frame = cap.read()
+frame =cv2.flip(frame, 1)
+blurred_frame = cv2.GaussianBlur(frame, (45,45), 0)
+
+hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+    
+lower_blue = np.array([38, 86, 0])
+upper_blue = np.array([121, 255, 255])
+mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    
+contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+
+
+
 
 while True:
 	diff = cv2.absdiff(prev, new)
 	diff = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
 	diff = cv2.blur(diff, (5,5))
+ 
 	_,thresh = cv2.threshold(diff, 10, 255, cv2.THRESH_BINARY)
-	threh = cv2.dilate(thresh, None, 3)
-	thresh = cv2.erode(thresh, np.ones((4,4)), 1)
+	threh = cv2.dilate(thresh, None, 3)  # type: ignore
+	thresh = cv2.erode(thresh, np.ones((4,4)), 1)  # type: ignore
+ 
 	contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-	cv2.circle(prev, (20,200), 5, (0,0,255), -1)
-	for contors in contor:				
+	cv2.circle(prev, (20,200), 5, (0,255,0), -1)
+	for contors in contours:				
 		if cv2.contourArea(contors) > 300:
 			(x,y,w,h) = cv2.boundingRect(contors)
 			(x1,y1),rad = cv2.minEnclosingCircle(contors)
@@ -32,11 +49,23 @@ while True:
 					
 	
 	cv2.imshow("orig", prev)
+	cv2.imshow("Mask", mask)
 	
 	prev = new
 	_, new = cap.read()
 	new = cv2.flip(new, 1)
+	blurred_frame=cv2.GaussianBlur(new, (45,45), 0)
+	hsv = cv2.cvtColor(blurred_frame, cv2.COLOR_BGR2HSV)
+	lower_blue = np.array([38, 86, 0])
+	upper_blue = np.array([121, 255, 255])
+	mask = cv2.inRange(hsv, lower_blue, upper_blue)
+	contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+	for contour in contours:
+		if cv2.contourArea(contour) > 1 :
+			cv2.drawContours(frame, contour, -1, (0, 255, 0), 1)
+
+	
 	if cv2.waitKey(1) == 27:
 		break
 

4f360feebf61d9965ebc05f2b63bd6ef77dc9dd1
Epsilon <101011193+KumarSampurn@users.noreply.github.com>
2022-11-05 12:41:30 +0530

    created drone_controller.py
    Test File to check the response of the drone by Arming and Taking off

---
+++ drone_controller.py
@@ -0,0 +1,45 @@
+from dronekit import connect, VehicleMode ,Vehicle , LocationLocal , LocationGlobal, LocationGlobalRelative
+import time
+
+vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
+
+def arm_and_takeoff(aTargetAltitude):
+    """
+    Arms vehicle and fly to aTargetAltitude.
+    """
+
+    print ("Basic pre-arm checks")
+    # Don't try to arm until autopilot is ready
+    while not vehicle.is_armable:
+        print (" Waiting for vehicle to initialise...")
+        time.sleep(1)
+
+    print ("Arming motors")
+    # Copter should arm in GUIDED mode
+    vehicle.mode    = VehicleMode("GUIDED")
+    vehicle.armed   = True
+
+    # Confirm vehicle armed before attempting to take off
+    while not vehicle.armed:
+        print (" Waiting for arming...")
+        time.sleep(1)
+
+    print ("Taking off!")
+    vehicle.simple_takeoff(aTargetAltitude) # Take off to target altitude
+
+    # Wait until the vehicle reaches a safe height before processing the goto (otherwise the command
+    #  after Vehicle.simple_takeoff will execute immediately).
+    while True:
+        print (" Altitude: ", vehicle.location.global_relative_frame.alt)
+        #Break and return from function just below target altitude.
+        if vehicle.location.global_relative_frame.alt>=aTargetAltitude*0.95:
+            print ("Reached target altitude")
+            break
+        time.sleep(1)
+
+arm_and_takeoff(20)
+
+
+
+
+
